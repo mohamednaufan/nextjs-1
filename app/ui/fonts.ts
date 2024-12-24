4efe77abfba489b61e9d7e00c9ac5a7b9f2df132
mohamed Nowfan <mohamednowfan30@gmail.com>
2024-12-24 19:25:04 +0530

    fonts update

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,11 @@ import { Inter, Oswald, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 export const oswald = Oswald({subsets: ['latin']})
-export const lusitana = Lusitana({weight: ['400']})
+
+const lusitana = Lusitana({
+  subsets: ['latin'], // Specify the 'latin' subset
+  weight: '400', // Add the desired weight if needed
+  preload: true,  // Preload if you still want to enable it
+});
+
+export default lusitana;
